9915ec96089577ee82da06521e273fdc8f0a78bb
Max Sviatukha <rubell2017max@gmail.com>
2022-01-26 00:49:14 +0200

    checkout

--- pages/[category].js
+++ pages/[category].js
@@ -1,39 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react";
 import { categories, getData, getUrl } from "../service";
-import styles from "../styles/itemList.module.scss"
-import broken from '../public/images/broken.jpeg'
+import styles from "../styles/itemList.module.scss";
+import Head from "next/head";
 
 
 const Category = ({list ,category}) => {
 
   return (
-    <div className={styles.mainBlock}>
-      <ul className={styles.itemsList}>
-        {list && list.map((item, index) => {
-          return (
-          <li className={styles.item} key={index} >
-            <Image 
-              className={styles.itemImage} 
-              src={getUrl(category, index)} 
-              width="100%"
-              height="100%"
-              layout='responsive'
-              objectFit="contain"
-              priority='false'
-              alt={item.name}
-              placeholder='blur'
-              blurDataURL='/images/broken.jpeg'
-            />
-            <Link className='itemName' href={`/${category}/${index}`}> 
-              <a>{category === "films" ? item.title : item.name}</a>
-            </Link>
-          </li>)
-        })}
-      </ul>
-    </div>
+    <>
+      <Head>
+        <title>{category}</title>
+      </Head>
+      <div className={styles.mainBlock}>
+        <ul className={styles.itemsList}>
+          {list && list.map((item, index) => {
+            return (
+            <li className={styles.item} key={index} >
+              <Image 
+                className={styles.itemImage} 
+                src={getUrl(category, index)} 
+                width={100}
+                height={100}
+                layout='responsive'
+                objectFit="contain"
+                priority='true'
+                alt={item.name}
+                placeholder='blur'
+                blurDataURL='/images/broken.jpeg'
+              />
+              <Link className='itemName' href={`/${category}/${index}`}> 
+                <a>{category === "films" ? item.title : item.name}</a>
+              </Link>
+            </li>)
+          })}
+        </ul>
+      </div>
+    </>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.scss'
 import Header from '../components/header/header'
 import Navbar from '../components/navbar/navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>SW</title>
+      </Head>
       <Header />
       <Navbar />
       <main>
